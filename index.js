e5da1c43b0b66c1cf80fe7d8091008c680d7d369
GannaKov <zlatta2000@gmail.com>
2023-12-16 11:40:11 +0100

    delete task

--- index.js
+++ index.js
@@ -12,13 +12,14 @@ function onSubmitClick(e) {
   const form = e.target;
   const taskName = form.elements.task_name;
   const task = form.elements.task;
-  console.log(taskName, task);
-  toDoList.push({ task: task.value, name: taskName.value });
-  console.log(toDoList);
-  renderTask();
-  console.log(form.elements);
-  taskName.value = "";
-  task.value = "";
+  if (taskName.value.trim() === "" || task.value.trim() === "") {
+    alert("Please fill the form before submitting.");
+  } else {
+    toDoList.push({ task: task.value, name: taskName.value });
+    renderTask();
+    taskName.value = "";
+    task.value = "";
+  }
 }
 
 function renderTask() {
@@ -30,8 +31,42 @@ function renderTask() {
               <h2 class="wbs-item__title">${item.name}</h2>
               <p class="wbs-item__text">${item.task}</p>
             </div>
+            <button type="button" class="wbs-btn__edit">Edit</button>
+            <button type="button" class="wbs-btn__delete">Delete</button>
           </li>`
     )
     .join("");
   list.insertAdjacentHTML("beforeend", markup);
+  addListener();
+  //   const editBtn = document.querySelector(".wbs-btn__edit");
+  //   const deleteBtn = document.querySelector(".wbs-btn__delete");
+}
+function addListener() {
+  if (toDoList.length != 0) {
+    const editBtns = document.querySelectorAll(".wbs-btn__edit");
+    const deleteBtns = document.querySelectorAll(".wbs-btn__delete");
+    const taskitems = document.querySelectorAll(".wbs-item");
+    taskitems.forEach((btn) => btn.addEventListener("click", onItemClick));
+    // console.log(deleteBtns);
+    // editBtns.forEach((btn) => btn.addEventListener("click", onEditBtnClick));
+    // console.log(deleteBtns);
+    // deleteBtns.forEach((btn) =>
+    //   btn.addEventListener("click", onDeleteBtnClick)
+    // );
+  }
+}
+
+function deleteTasks(item) {
+  item.remove();
+}
+function onEditBtnClick() {
+  console.log("Edit btn");
+}
+function onItemClick(e) {
+  console.log(e.target.className);
+  console.log("target", e.target);
+  console.log("cur target", e.currentTarget);
+  if (e.target.className === "wbs-btn__delete") {
+    deleteTasks(e.currentTarget);
+  }
 }
